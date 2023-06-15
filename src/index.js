47f54b4fa9e3935047d3ec1b665342a23692246b
gabriellenon <47499187+gabriel8824@users.noreply.github.com>
2023-06-14 21:04:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3333;
